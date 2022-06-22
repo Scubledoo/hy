857e065d9a7796272df42e5ef41d2792f9ab61d9
Scubledoo <101173548+Scubledoo@users.noreply.github.com>
2022-06-22 22:10:47 +0800

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 //=======『 Tampilan Dan Lainnya 』============//
 global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti jadi foto bot mu
 global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
-global.lolkey = 'Papah-chan' //biar mudah ngegantinya semisal apikeynya expired:v
+global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = 'Necto-𝙼𝙳'
 global.watermark = wm
