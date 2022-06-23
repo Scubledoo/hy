9ed1b5319e1bd6a7972177786b2e25a41307d044
Scubledoo <101173548+Scubledoo@users.noreply.github.com>
2022-06-23 15:07:59 +0800

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ global.pulsa = '-'
 global.gopay = '-'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'YhaBot-Md'
+global.namebot = 'NectoBot-Md'
 global.gc = 'https://chat.whatsapp.com/KE9VFp6sMLpJcOYDnkG1EP'
 global.web = 'https://hyzerr-apii.herokuapp.com' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
